625364d782a93eb22f147aef5f5a1b8f42e3a74e
weiweivita <xiarifengdong2007@qq.com>
2016-06-21 18:05:45 +0800

    ui-router

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 ﻿(function () {
     'use strict';
 
-    angular.module('app', ['ngRoute'])
-    .controller("aController",function($scope){
-        console.log($scope);
-    })
+    angular.module('app', ['ngRoute','ui.router'])
 	.controller("EfuiCtrl",function($scope,$location){	
+		var pathes = ['/component', '/element', '/plugin', '/standard', '/tools'];
 		$scope.$on("$viewContentLoaded",function(){
 	    	console.log("ng-view content loaded!");
 		});	
 	 	$scope.$on("$routeChangeStart",function(event,next,current){
 	     	console.log("route change start!");
-	     	console.log($location.path());
-	     	console.log(next);
-	     	if($location.path()==='')console.log($location);
-	  	});
+	     	var _path = $location.path();
+	     	if(pathes.indexOf(_path)!=-1){
+	     		
+	     		console.log($location);
+	     		//$location.path("/index1");
+				//return;
+	     	}
+	     	
+	  });
 	})
  	.config(config);
 
@@ -23,8 +26,8 @@
     	var pages = ['component', 'element', 'plugin', 'standard', 'tools'];
 
 		var setRoutes = function(route) {
-			var   l =  route.indexOf("/");
-			var	view = l!=-1  ? route: route+'/index';
+			var   l =  route.indexOf("/:");
+			var	view = l!=-1  ? route.substring(0,l): route+'/index';
 			var tempurl, url;
 			url = '/' + route;
 			tempurl = {
@@ -39,8 +42,35 @@
 		});
         $routeProvider
      	.when('/', {
-         	templateUrl: 'common/main.html',
-            controller: 'aController'
+         	templateUrl: 'common/main.html'
+   		})
+     	.when('/component/:name', {
+         	templateUrl: function($routeParams){return 'component/'+$routeParams.name+'.html';}
+   		})
+     	.when('/element/:name', {
+         	templateUrl: function($routeParams){return 'element/'+$routeParams.name+'.html';},
+         	controller: function($scope,$location){
+         		$scope.docsTit="基本元素";
+         		$scope.docsMsg="CSS样式、HTML标签";
+         		console.log($scope);
+         	},
+         	resolve:{ 
+				deps: [ '$ocLazyLoad', 
+				function ($ocLazyLoad) { 
+					console.log($ocLazyLoad);
+					//return $ocLazyLoad.load([appJsPath + '/controller/follower/list.js', appVendorPath + '/smarttable/smarttable.js']);
+				 	}
+				]
+	        }
+   		})
+     	.when('/plugin/:name', {
+         	templateUrl: function($routeParams){return 'plugin/'+$routeParams.name+'.html';}
+   		})
+     	.when('/standard/:name', {
+         	templateUrl: function($routeParams){return 'standard/'+$routeParams.name+'.html';}
+   		})
+     	.when('/tools/:name', {
+         	templateUrl: function($routeParams){return 'tools/'+$routeParams.name+'.html';}
    		})
       	.otherwise({ redirectTo: '/index.html' });
     }
