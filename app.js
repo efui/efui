90c639d399e60a26e393d1cafb6de963bdb0fcf4
vitaoa <jupiterzw@sina.com>
2017-02-21 17:46:04 +0800

    menulist 完善

--- app.js
+++ app.js
@@ -5,142 +5,20 @@
 
 	var app = angular.module('app', ['ui.router']);
 	
-	//directive定义
-	app.directive('headerpanel', function () {
-	 	return { 
-	 		restrict: 'EA', 
-	 		templateUrl: 'common/header.html'
-	 	}; 
-	}); 
-	app.directive('footerpanel', function () {
-	 	return { 
-	 		restrict: 'EA', 
-	 		templateUrl: 'common/footer.html'
-	 	}; 
-	}); 
-//	app.directive('tabpanel', function () {
-//	    return {
-//	    	restrict: 'A'
-//	    };
-//	});
-	app.directive('my-class', function () {
-	    return {
-	    	restrict: 'C'
-	    };
-	});
-	app.directive('panel', function () {
-	    return {
-	    	restrict: 'E',
-	 		replace: true,
-	        template: '<div><div ng-transclude class="sidebar-slide-overlay"></div></div>',
-	        transclude: true,
-	        link:function(scope){
-	        	scope.things = [11,22,33];
-				scope.data =  [
-				    {
-				        str: '表单a'
-				    },
-				    {
-				        str: '表单b'
-				    },
-				    {
-				        str: '表单c'
-				    }
-				]; 
-	        }
-	    };
-	});
-	app.directive('expander', function () {
-	    return {
-	    	restrict: 'EA',
-	    	transclude : true,
-	 		replace: true,
-	        template : '<div ng-transclude></div>',
-	        link : function(scope, element, attrs) {
-	            scope.showMe = false;
-				scope.title = '点击展开';
-				scope.title2 = '点击缩回';
-				scope.text = '这里是内部的内容。';
-				scope.data =  [
-				    {
-				        str: '表单a'
-				    },
-				    {
-				        str: '表单b'
-				    },
-				    {
-				        str: '表单c'
-				    }
-				]; 
-				scope.className = function() {
-			        var className = 'initClass';	
-			        var _num = parseInt(scope.$index);
-			        if (++_num)
-			            className += _num;			
-			        return className;
-			    };
-//				console.log(scope.$index);
-	            scope.toggle = function toggle() {
-	                scope.showMe = !scope.showMe;
-	            }
-	        }
-	    };
-	});	
-	app.directive('helloWorld', function() { 
-	 	return { 
-	 		restrict: 'E', 
-	 		templateUrl: 'hello.html', 
-	 		replace: true 
-	 	}; 
-	}); 
-	app.directive("helloWorld2",function(){ 
-	 	return{ 
-	 		restrict:'EAC', 
-	 		transclude: true, //注意此处必须设置为true 
-	 		controller: function ($scope, $element,$attrs,$transclude,$log) { //在这里你可以注入你想注入的服务 
-			  	$transclude(function (clone) { 
-			   		var a = angular.element('<p>'); 
-//			   		a.css('color', $attrs.mycolor); 
-			   		a.text(clone.text()); 
-//			   		$element.append(a); 
-//			   		var a = $transclude(); //$transclude()就是嵌入的内容
-			   		a.css('color', $attrs.mycolor); 
-  					$element.append(a); 
-//			  		$log.info(clone); 
-			  	}); 
-//			  	$log.info("hello everyone"); 
-	        } 
-	 	}; 
-	}); 
-	app.directive('myDirective', function () { 
-	 	return { 
-		 	restrict: 'EA', 
-		 	scope:true, 
-		 	template: '<div>儿子:{{ name }}<input ng-model="name"/></div>' 
-		}; 
-	});
-	app.directive('multipleEmail', [function () {
-      	return {
-          	require: "ngModel",
-          		link: function (scope, element, attr, ngModel) {
-              	if (ngModel) {
-                  	var emailsRegexp = /^([a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*[;；]?)+$/i;
-              	}
-              	var customValidator = function (value) {
-                  	var validity = ngModel.$isEmpty(value) || emailsRegexp.test(value);
-                  	ngModel.$setValidity("multipleEmail", validity);
-                  	return validity ? value : undefined;
-              	};
-              	ngModel.$formatters.push(customValidator);
-              	ngModel.$parsers.push(customValidator);
-          	}
-		};
-	}]);
 	
 	/* 缓存模板templateUrl */
 	app.run(["$templateCache", function($templateCache) {
 	 	$templateCache.put("hello.html", "<div><h1>Hi 我是林炳文~~~6666</h1></div>"); 
 	 	$templateCache.put("hello2.html", "<div><h1>Hi 我是林炳文~~~8888</h1></div>"); 
+	 	$templateCache.put("toptitle.html", '<h3 class="mb10" ng-repeat="data in menulist">'+
+			'<span ng-repeat="item in data.likes">'+
+				'<span class="red" ng-if="!item.submenu && item.title == pagename">{{item.title}}</span>'+
+				'<span ng-if="item.submenu" ng-repeat="subitem in item.submenu">'+
+					'<span ng-if="pagename == subitem.url">{{subitem.name}}</span>'+
+				'</span>'+
+			'</span>'+
+		'</h3>'); 
+	 	
 	}]); 
 	app.controller('MainController', function ($scope) { 
 	 	$scope.name = '林炳文'; 
@@ -227,34 +105,42 @@
 				url: "/angular",
 				templateUrl: "angular/index.html",
 				controller: function($scope, $location) {
-					$scope.docsTit = "angular专区";
-					$scope.docsMsg = "angularJs实例演示"; 
-					$scope.departments = [  
+					$scope.docsTit = "前端框架库";
+					$scope.docsMsg = "以Javascript语言为基础搭建的编程框架"; 
+					$scope.isShow = false; 
+					$scope.menulist = [  
 					    {  
-					        "firstName": "单页面1", 
+					        "firstName": "Angular", 
 					        "likes": [
 					        	{
-					        		'title':"11111",
+					        		'title':"SPA",
 					        		'submenu':[
-					        			{'name':"子页面1",'url':"angular-spa1"},
+					        			{'name':"表单验证",'url':"angular-form-validate"},
 					        			{'name':"子页面2",'url':"angular-spa2"},
-					        			{'name':"子页面3",'url':"angular-spa3"},
-					        			{'name':"子页面4",'url':"angular-spa4"}
+					        			{'name':"子页面3",'url':"angular-spa3"}
 					        		]
 					        	}
 					        ]
 					    },  
 					    {  
-					        "firstName": "单页面2", 
+					        "firstName": "Vue", 
 					        "likes": [
 					        	{
-					        		'title':"2222",
+					        		'title':"vue",
 					        		'submenu':[
-					        			{'name':"子页面3",'url':"angular-spa3"},
-					        			{'name':"子页面4",'url':"angular-spa4"}
+					        			{'name':"vue入门",'url':"vue"}
 					        		]
 					        	}
 					        ]
+					    },
+					    {
+					    	"firstName":"Backbone",
+					    	"likes":[
+					    		{
+					    			'title':'backbone',
+					    			'url':'backbone'
+					    		}
+					    	]
 					    }
 					];
 					$scope.submitForm = function(isValid) {
@@ -266,7 +152,13 @@
 			})
 			.state('angular.pages', {
                 url: '/:pageid',
-				templateUrl:function($routeParams){return 'angular/'+$routeParams.pageid+'.html';}
+				templateUrl:function($routeParams){return 'angular/'+$routeParams.pageid+'.html';},
+				controller: function($scope, $location,$state,$stateParams,$element) {
+					$scope.pagename = $stateParams.pageid;
+//					console.log($element);
+//					console.log($state);
+//					console.log($stateParams);
+				}
 			})
 	}
 
@@ -311,4 +203,137 @@
 			}
 		})
 	}
+	
+	
+	//directive定义
+	app.directive('headerpanel', function () {
+	 	return { 
+	 		restrict: 'EA', 
+	 		templateUrl: 'common/header.html'
+	 	}; 
+	}); 
+	app.directive('footerpanel', function () {
+	 	return { 
+	 		restrict: 'EA', 
+	 		templateUrl: 'common/footer.html'
+	 	}; 
+	}); 
+	app.directive('my-class', function () {
+	    return {
+	    	restrict: 'C'
+	    };
+	});
+	app.directive('panel', function () {
+	    return {
+	    	restrict: 'E',
+	 		replace: true,
+	        template: '<div><div ng-transclude class="sidebar-slide-overlay"></div></div>',
+	        transclude: true,
+	        link:function(scope){
+	        	scope.things = [11,22,33];
+				scope.data =  [
+				    {
+				        str: '表单a'
+				    },
+				    {
+				        str: '表单b'
+				    },
+				    {
+				        str: '表单c'
+				    }
+				]; 
+	        }
+	    };
+	});
+	app.directive('expander', function () {
+	    return {
+	    	restrict: 'EA',
+	    	transclude : true,
+	 		replace: true,
+	        template : '<div ng-transclude></div>',
+	        link : function(scope, element, attrs) {
+	            scope.showMe = false;
+				scope.title = '点击展开';
+				scope.title2 = '点击缩回';
+				scope.text = '这里是内部的内容。';
+				scope.data =  [
+				    {
+				        str: '表单a'
+				    }
+				]; 
+				scope.className = function() {
+			        var className = 'initClass';	
+			        var _num = parseInt(scope.$index);
+			        if (++_num)
+			            className += _num;			
+			        return className;
+			    };
+//				console.log(scope.$index);
+	            scope.toggle = function toggle() {
+	            	console.log('toggle me!');
+	                scope.showMe = !scope.showMe;
+	            }
+	        }
+	    };
+	});	
+	
+	app.directive('topTitle', function() {
+	 	return { 
+	 		restrict: 'EA', 
+		 	scope:true, 
+	 		templateUrl: 'toptitle.html', 
+	 		replace: true 
+	 	}; 
+	}); 
+	app.directive('helloWorld', function() { 
+	 	return { 
+	 		restrict: 'EA', 
+		 	scope:true, 
+	 		templateUrl: 'hello.html', 
+	 		replace: true 
+	 	}; 
+	}); 
+	app.directive("helloWorld2",function(){ 
+	 	return{ 
+	 		restrict:'EAC', 
+	 		transclude: true, //注意此处必须设置为true 
+	 		controller: function ($scope, $element,$attrs,$transclude,$log) { //在这里你可以注入你想注入的服务 
+			  	$transclude(function (clone) { 
+			   		var a = angular.element('<p>'); 
+//			   		a.css('color', $attrs.mycolor); 
+			   		a.text(clone.text()); 
+//			   		$element.append(a); 
+//			   		var a = $transclude(); //$transclude()就是嵌入的内容
+			   		a.css('color', $attrs.mycolor); 
+  					$element.append(a); 
+//			  		$log.info(clone); 
+			  	}); 
+//			  	$log.info("hello everyone"); 
+	        } 
+	 	}; 
+	}); 
+	app.directive('myDirective', function () { 
+	 	return { 
+		 	restrict: 'EA', 
+		 	scope:true, 
+		 	template: '<div>儿子:{{ name }}<input ng-model="name"/></div>' 
+		}; 
+	});
+	app.directive('multipleEmail', [function () {
+      	return {
+          	require: "ngModel",
+          		link: function (scope, element, attr, ngModel) {
+              	if (ngModel) {
+                  	var emailsRegexp = /^([a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*[;；]?)+$/i;
+              	}
+              	var customValidator = function (value) {
+                  	var validity = ngModel.$isEmpty(value) || emailsRegexp.test(value);
+                  	ngModel.$setValidity("multipleEmail", validity);
+                  	return validity ? value : undefined;
+              	};
+              	ngModel.$formatters.push(customValidator);
+              	ngModel.$parsers.push(customValidator);
+          	}
+		};
+	}]);
 });
